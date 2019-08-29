80c1e6cb6b41764412000d6c40ca3fc3cf1195fa
Michael O'Donnell <gorillaejones@gmail.com>
2019-08-29 11:01:18 -0500

    dependencies added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+function* watcherSaga() {
+    //takeevery goes here
+}
+
+const store = createStore(
+    combineReducers({
+        //reducers need to be called in here to work
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+)
+
+const sagaMiddleware = createSagaMiddleware();
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
