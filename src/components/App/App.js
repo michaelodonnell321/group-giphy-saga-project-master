676371b70551ac4723eb05f8c59d565fa51053e1
Michael O'Donnell <gorillaejones@gmail.com>
2019-08-29 15:59:28 -0500

    router and route paths added to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Favorites from '../Favorites/Favorites'
 
 
@@ -8,11 +9,13 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
       <div>
         <Header />
-        <Home />
-        <Favorites />
+        <Route path='/' exact component={Home} />
+        <Route path='/favorites' component={Favorites} />
       </div>
+      </Router>
     );
   }
   
