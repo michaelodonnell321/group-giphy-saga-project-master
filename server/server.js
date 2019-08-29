40f7bf3b8fa6532abcfc9607019ee88f9854d8ff
pachevang <vangpache@gmail.com>
2019-08-29 11:51:53 -0500

    add api setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+process.env.API_KEY
 
 // Route includes
 const favoriteRouter = require('./routes/favorite.router');
