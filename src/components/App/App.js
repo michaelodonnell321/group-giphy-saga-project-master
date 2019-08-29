7eeb64451cd52cb67b0a14788ca4998f3a055181
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-08-29 11:57:15 -0500

    initial component set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Favorites from '../Favorites/Favorites'
 
 
 class App extends Component {
@@ -6,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Giphy Search!</h1>
+        <Header />
+        <Home />
+        <Favorites />
       </div>
     );
   }
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+    render() {
+        return (
+            <h2>Favorites Component</h2>
+        )
+    }
+}
+
+export default Favorites;
